2a786e521ac3cfc6d45584ef84056bdd757fba75
Adam DiCarlo <adam@bikko.org>
2023-08-01 09:21:52 -0700

    Add substituters

--- flake.nix
+++ flake.nix
@@ -7,11 +7,16 @@
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [
-      # Nix community's cache server
       "https://nix-community.cachix.org"
+      "https://fufexan.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://cache.privatevoid.net"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
     ];
   };
 

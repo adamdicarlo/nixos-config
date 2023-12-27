0b5d4d3313956a5ff7039fb86f3ec92e65d08388
Adam DiCarlo <adam@bikko.org>
2023-12-26 21:30:38 -0800

    Refactor to make it easy to add tons of simple traefik services

--- machines/opti/traefik.nix
+++ machines/opti/traefik.nix
@@ -3,21 +3,35 @@
 
   services.traefik = let
     certDir = config.security.acme.certs."sleeping-panda.net".directory;
+
+    mkSimpleRouters = services:
+      builtins.listToAttrs (
+        builtins.map (
+          service: {
+            name = service;
+            value = {
+              inherit service;
+              rule = "Host(`${service}.sleeping-panda.net`)";
+              entryPoints = "websecure";
+              tls = true;
+            };
+          }
+        )
+        services
+      );
   in {
     enable = true;
     dynamicConfigOptions = {
-      http.routers.adguardhome = {
-        rule = "Host(`adguardhome.sleeping-panda.net`)";
-        entryPoints = "websecure";
-        service = "adguardhome";
-        tls = true;
-      };
-      http.routers.traefik = {
-        rule = "Host(`traefik.sleeping-panda.net`)";
-        entryPoints = "websecure";
-        service = "api@internal";
-        tls = true;
-      };
+      http.routers =
+        mkSimpleRouters ["adguardhome"]
+        // {
+          traefik = {
+            rule = "Host(`traefik.sleeping-panda.net`)";
+            entryPoints = "websecure";
+            service = "api@internal";
+            tls = true;
+          };
+        };
       http.services.adguardhome = {
         loadBalancer.servers = [{url = "http://localhost:5300/";}];
       };

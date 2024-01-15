a2ad1c334fcaf39405a39c58d5236201c2492299
Adam DiCarlo <adam@bikko.org>
2024-01-15 11:16:06 -0800

    openssh: Turn on AllowAgentForwarding

--- machines/common.nix
+++ machines/common.nix
@@ -76,7 +76,12 @@
   services.fstrim.enable = true;
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      AllowAgentForwarding = true;
+    };
+  };
 
   security.sudo.wheelNeedsPassword = false;
 

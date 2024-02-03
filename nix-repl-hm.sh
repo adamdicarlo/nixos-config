c07c95c490e999099b337c9a6afce4782bca11ac
Adam DiCarlo <adam@bikko.org>
2024-02-03 15:29:03 -0800

    Better home-manager repl script

--- nix-repl-hm.sh
+++ nix-repl-hm.sh
@@ -1,16 +1,7 @@
 #/bin/bash
-set -e
-CODE="$(
-	cat <<EOF
-let
-  hm = import <home-manager/modules> { configuration = ~/nixos-config/home.nix; pkgs = import <nixpkgs> {}; };
-in
-{ inherit hm; }
-EOF
-)"
-tmp=$(mktemp)
-printf '%s\n' "$CODE" >"$tmp"
-
-echo "$CODE"
-NIX_PATH=nixpkgs=http://nixos.org/channels/nixos-unstable/nixexprs.tar.xz:home-manager=https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz \
-	nix repl --file "$tmp"
+echo "Useful paths:"
+echo
+echo '- homeConfigurations."adam@tiv".config'
+echo '- homeConfigurations."adam@tiv".options'
+echo
+nix repl .

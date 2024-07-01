eb0e6be33ccb3628bed19d3214dc11073e1bb45c
Adam DiCarlo <adam@bikko.org>
2024-06-30 17:37:05 -0700

    home-manager: Extract kanshi config

---
+++ home-manager/modules/kanshi.nix
@@ -0,0 +1,63 @@
+{
+  services.kanshi = {
+    enable = true;
+    settings = [
+      {
+        profile.name = "dell-ultrawide";
+        profile.outputs = [
+          {
+            criteria = "Dell Inc. DELL U3821DW HH7YZ63";
+            status = "enable";
+            mode = "3840x1600@60Hz";
+            position = "0,0";
+            scale = 1.0;
+          }
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+        ];
+      }
+      {
+        profile.name = "lg-ultrawide";
+        profile.outputs = [
+          {
+            criteria = "Goldstar Company Ltd LG HDR WQHD 0x0000B6E2";
+            status = "enable";
+            mode = "3440x1440@60Hz";
+            position = "0,0";
+            scale = 1.0;
+          }
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+        ];
+      }
+      {
+        profile.name = "x1c7-undocked";
+        profile.outputs = [
+          {
+            criteria = "Unknown 0x07C8 0x00000000";
+            status = "enable";
+            mode = "3840x2160@60Hz";
+            position = "0,0";
+            scale = 2.0;
+          }
+        ];
+      }
+      {
+        profile.name = "addw3-undocked";
+        profile.outputs = [
+          {
+            criteria = "BOE 0x08B3";
+            status = "enable";
+            mode = "1920x1080@144Hz";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+      }
+    ];
+  };
+}

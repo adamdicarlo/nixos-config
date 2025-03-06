283ec185e14e9ca25eec37f0bb67c92eaba5327b
Adam DiCarlo <adam@bikko.org>
2025-03-05 17:58:08 -0800

    Fix sysctl config 😅

--- machines/common.nix
+++ machines/common.nix
@@ -29,11 +29,9 @@
   boot.kernel.sysctl = {
     # Increase the amount of inotify watchers
     # Note that inotify watches consume 1kB on 64-bit machines.
-    kernel.sysctl = {
-      "fs.inotify.max_user_watches" = 64 * 1024; # default:  8192
-      "fs.inotify.max_user_instances" = 1024; # default:   128
-      "fs.inotify.max_queued_events" = 32768; # default: 16384
-    };
+    "fs.inotify.max_user_watches" = 64 * 1024; # default:  8192
+    "fs.inotify.max_user_instances" = 1024; # default:   128
+    "fs.inotify.max_queued_events" = 32768; # default: 16384
   };
 
   # Set limits for systemd units (not systemd itself).
